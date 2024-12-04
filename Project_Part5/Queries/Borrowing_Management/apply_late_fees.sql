b948ad6efef347b96354f72bd62427399ba03ccc
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-03 23:02:41 -0600

    Update apply_late_fees.sql

--- Project_Part5/Queries/Borrowing_Management/apply_late_fees.sql
+++ Project_Part5/Queries/Borrowing_Management/apply_late_fees.sql
@@ -1,8 +1,10 @@
 UPDATE Borrowing
-SET lateFee = GREATEST(0, (DATEDIFF(returnDate, dueDate) * (SELECT baseFee FROM FeePolicy WHERE policyID = Borrowing.policyID)))
+SET lateFee = GREATEST(0, DATEDIFF(returnDate, dueDate) * 
+    (SELECT baseFee FROM FeePolicy WHERE policyID = Borrowing.policyID))
 WHERE returnDate > dueDate
 AND EXISTS (
     SELECT 1
     FROM User
-    WHERE userID = <adminUserID> AND role = 'Admin'
+    WHERE userID = 1 AND role = 'Admin'  -- Replace 1 with the actual admin user ID
 );
+
