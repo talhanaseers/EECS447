acee6029072d2c3469a0b648ba9a05dc689308d9
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-04 00:29:36 -0600

    removed a limit

--- Project_Part5/Queries/Search_and_Reports/resource_usage_report.sql
+++ Project_Part5/Queries/Search_and_Reports/resource_usage_report.sql
@@ -2,5 +2,4 @@ SELECT r.title, COUNT(b.borrowID) AS timesBorrowed
 FROM Borrowing b
 JOIN Resource r ON b.resourceID = r.resourceID
 GROUP BY r.title
-ORDER BY timesBorrowed DESC
-LIMIT 10;
+ORDER BY timesBorrowed DESC;
