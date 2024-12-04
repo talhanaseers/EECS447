74bb71a13c0930e40b8674a43acb33976dd0ee6b
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-03 23:31:52 -0600

    Update remove_resource.sql

--- Project_Part5/Queries/Resource_Management/remove_resource.sql
+++ Project_Part5/Queries/Resource_Management/remove_resource.sql
@@ -1,2 +1,13 @@
+--This wont work as Resource ID is a foreign key in the borrowing table
+
+--DELETE FROM Resource
+--WHERE resourceID = 1001;
+
+--Here is a more accurate functionality to delete a resource
+-- Delete dependent rows from the Borrowing table first
+DELETE FROM Borrowing
+WHERE resourceID = 1001;
+
+-- Now delete the resource from the Resource table
 DELETE FROM Resource
 WHERE resourceID = 1001;
