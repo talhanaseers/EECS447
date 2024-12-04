277de71f9e93516e50e43db392f889945192bcd2
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-03 21:44:40 -0600

    added a limit to the report size

--- Project_Part5/Queries/Search_and_Reports/resource_usage_report.sql
+++ Project_Part5/Queries/Search_and_Reports/resource_usage_report.sql
@@ -2,4 +2,5 @@ SELECT r.title, COUNT(b.borrowID) AS timesBorrowed
 FROM Borrowing b
 JOIN Resource r ON b.resourceID = r.resourceID
 GROUP BY r.title
-ORDER BY timesBorrowed DESC;
+ORDER BY timesBorrowed DESC
+LIMIT 10;
