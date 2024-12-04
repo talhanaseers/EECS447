b083c0b527473f7c010955d11170c0df7222fea7
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-03 21:40:38 -0600

    added a limit to the report size

--- Project_Part5/Queries/Search_and_Reports/overdue_items_report.sql
+++ Project_Part5/Queries/Search_and_Reports/overdue_items_report.sql
@@ -2,4 +2,5 @@ SELECT b.borrowID, u.name AS userName, r.title, b.dueDate, DATEDIFF(CURRENT_DATE
 FROM Borrowing b
 JOIN User u ON b.userID = u.userID
 JOIN Resource r ON b.resourceID = r.resourceID
-WHERE b.returnDate IS NULL AND b.dueDate < CURRENT_DATE;
+WHERE b.returnDate IS NULL AND b.dueDate < CURRENT_DATE
+  LIMIT 10; --Only prints the first 10 results for testing
