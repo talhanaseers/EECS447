1be61c5efe7b1ef67eeebbe0be3711c578ce95e2
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-08 12:29:02 -0600

    Update Members_with_Overdue_Books.sql

--- Project_Part5/Additional Queries (Assigned December 5)/Members_with_Overdue_Books.sql
+++ Project_Part5/Additional Queries (Assigned December 5)/Members_with_Overdue_Books.sql
@@ -1,1 +1,19 @@
+SELECT 
+    u.userID,
+    u.name AS borrowerName,
+    u.email,
+    r.title AS overdueBookTitle,
+    b.dueDate
+FROM 
+    Borrowing b
+JOIN 
+    User u ON b.userID = u.userID
+JOIN 
+    Resource r ON b.resourceID = r.resourceID
+WHERE 
+    r.type = 'Book' 
+    AND b.returnDate IS NULL -- Book has not been returned
+    AND b.dueDate < CURDATE() -- Due date has passed
+ORDER BY 
+    u.userID, b.dueDate ASC;
 
