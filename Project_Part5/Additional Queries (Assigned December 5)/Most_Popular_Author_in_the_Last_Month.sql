7bd9bbd5303d772aa488065c51ee89cf8e54072a
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-08 12:31:03 -0600

    Update Most_Popular_Author_in_the_Last_Month.sql

--- Project_Part5/Additional Queries (Assigned December 5)/Most_Popular_Author_in_the_Last_Month.sql
+++ Project_Part5/Additional Queries (Assigned December 5)/Most_Popular_Author_in_the_Last_Month.sql
@@ -1,1 +1,18 @@
+SELECT 
+    a.authorID,
+    a.name AS authorName,
+    COUNT(b.borrowID) AS borrowCount
+FROM 
+    Borrowing b
+JOIN 
+    Resource r ON b.resourceID = r.resourceID
+JOIN 
+    Author a ON r.authorID = a.authorID
+WHERE 
+    b.borrowDate >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+GROUP BY 
+    a.authorID, a.name
+ORDER BY 
+    borrowCount DESC
+LIMIT 1;
 
