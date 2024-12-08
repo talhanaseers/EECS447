4d8795d16876d53e9e050981455f9c9d8dfe1f35
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-08 12:30:01 -0600

    Update Average_Borrowing_Time.sql

--- Project_Part5/Additional Queries (Assigned December 5)/Average_Borrowing_Time.sql
+++ Project_Part5/Additional Queries (Assigned December 5)/Average_Borrowing_Time.sql
@@ -1,1 +1,12 @@
+SELECT 
+    AVG(DATEDIFF(
+        COALESCE(b.returnDate, CURDATE()), -- Use the return date or the current date for ongoing borrowings
+        b.borrowDate
+    )) AS avgBorrowingDays
+FROM 
+    Borrowing b
+JOIN 
+    Resource r ON b.resourceID = r.resourceID
+WHERE 
+    r.type = 'Book';
 
