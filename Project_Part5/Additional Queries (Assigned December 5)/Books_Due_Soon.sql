e95d6fed1104afbfbec1f818a18d56404e5f4c16
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-12-08 12:28:21 -0600

    Update Books_Due_Soon.sql

--- Project_Part5/Additional Queries (Assigned December 5)/Books_Due_Soon.sql
+++ Project_Part5/Additional Queries (Assigned December 5)/Books_Due_Soon.sql
@@ -1,1 +1,19 @@
+SELECT 
+    b.borrowID,
+    r.resourceID,
+    r.title,
+    u.userID,
+    u.name AS borrowerName,
+    b.dueDate
+FROM 
+    Borrowing b
+JOIN 
+    Resource r ON b.resourceID = r.resourceID
+JOIN 
+    User u ON b.userID = u.userID
+WHERE 
+    r.type = 'Book'
+    AND b.dueDate BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)
+ORDER BY 
+    b.dueDate ASC;
 
